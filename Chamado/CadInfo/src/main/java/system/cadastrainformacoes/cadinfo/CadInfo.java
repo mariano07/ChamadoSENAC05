3a386013256785a46771868552c1e7307e99cf12
Arthur <arthurrapach06@gmail.com>
2022-05-13 20:30:30 -0300

    TESTE

--- Chamado/CadInfo/src/main/java/system/cadastrainformacoes/cadinfo/CadInfo.java
+++ Chamado/CadInfo/src/main/java/system/cadastrainformacoes/cadinfo/CadInfo.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package system.cadastrainformacoes.cadinfo;
 
-/**
- *
- * @author Aluno
- */
-public class CadInfo {
+import javax.swing.JOptionPane;
 
+public class CadInfo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JOptionPane.showMessageDialog(null,"TESTE");
     }
 }
