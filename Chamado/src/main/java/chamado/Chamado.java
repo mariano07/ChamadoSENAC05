21ead8aa67efd2fd6030ee5dc409b22cd563485f
Cristopher Machado <cristopherpiussi@gmail.com>
2022-05-20 21:07:38 -0300

    Update Chamado.java

--- Chamado/src/main/java/chamado/Chamado.java
+++ Chamado/src/main/java/chamado/Chamado.java
@@ -1,8 +1,10 @@
 package chamado;
+import cadastra_informacoes.Cad_Info;
+import cadastra_maquinas.CadastroMaquinas;
 
 public class Chamado {
 
     public static void main(String[] args) {
-        
+       
     }
 }
