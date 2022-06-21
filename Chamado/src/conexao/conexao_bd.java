7007964fb851f81eddc6a863e0a8e037cfc3d562
Victor-Barbosa06 <victorfb2001@gmail.com>
2022-06-21 19:58:20 -0300

    mudancas

--- Chamado/src/conexao/conexao_bd.java
+++ Chamado/src/conexao/conexao_bd.java
@@ -280,5 +280,20 @@ public class conexao_bd{
         desconectar();
         return verifica;
     }
-    
+    public String[] getAluno(String matricula){
+        String[] aluno = new String[2];
+        String InstrucaoSQL="SELECT Nome,Usuario FROM Usuario WHERE Matricula = "+matricula;
+        conectar();
+        try{
+            st = conexao.createStatement();
+            result = st.executeQuery(InstrucaoSQL);
+            aluno[0]=result.getString("Nome");
+            aluno[1]=result.getString("Usuario");
+        }catch(Exception e){
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "ERRO:C283", "ERRO", JOptionPane.ERROR_MESSAGE);
+        }
+        desconectar();
+        return aluno;
+    }
 }
