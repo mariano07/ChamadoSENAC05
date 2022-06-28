88be0f36d502acc9f6a4d4d999c7f7a5dfd13fd2
Victor-Barbosa06 <victorfb2001@gmail.com>
2022-06-28 20:24:58 -0300

    mudancas

--- Chamado/src/conexao/JConsultaChamado.java
+++ Chamado/src/conexao/JConsultaChamado.java
@@ -27,36 +27,45 @@ public class JConsultaChamado extends javax.swing.JInternalFrame {
     
     public JConsultaChamado() {
         initComponents();
+        Hidden();
     }
     private void Hidden(){
-        
+        box_Matricula.setVisible(false);
+        box_Patrimonio.setVisible(false);
+        box_Data.setVisible(false);
+        box_Status.setVisible(false);
+        invalidate();
+        validate();
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        JPanelConsulaChamado = new javax.swing.JPanel();
         Button_Alterar = new javax.swing.JButton();
         text_Consulta = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         List_Pendencias = new javax.swing.JList<>();
         check_FIltros = new javax.swing.JCheckBox();
-        check_Consultas = new javax.swing.JCheckBox();
         box_Patrimonio = new javax.swing.JComboBox<>();
         box_Matricula = new javax.swing.JComboBox<>();
         box_Data = new javax.swing.JComboBox<>();
-        box_status = new javax.swing.JComboBox<>();
+        box_Status = new javax.swing.JComboBox<>();
 
+        setBorder(null);
         setClosable(true);
         setIconifiable(true);
 
-        Button_Alterar.setText("Alternar lista");
+        JPanelConsulaChamado.setBorder(javax.swing.BorderFactory.createTitledBorder("Pesquisar chamados"));
+
+        Button_Alterar.setText("Alterar");
         Button_Alterar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 Button_AlterarMouseClicked(evt);
             }
         });
 
-        text_Consulta.setText("Pesquisar...                       ");
+        text_Consulta.setText("Pesquisar...");
         text_Consulta.setPreferredSize(new java.awt.Dimension(236, 25));
         text_Consulta.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
@@ -67,19 +76,13 @@ public class JConsultaChamado extends javax.swing.JInternalFrame {
             }
         });
 
+        List_Pendencias.setBorder(javax.swing.BorderFactory.createTitledBorder("Selecione um chamado para editar"));
         jScrollPane1.setViewportView(List_Pendencias);
 
         check_FIltros.setText("Filtros");
         check_FIltros.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 check_FIltrosStateChanged(evt);
-            }
-        });
-
-        check_Consultas.setText("Consultar");
-        check_Consultas.addChangeListener(new javax.swing.event.ChangeListener() {
-            public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                check_ConsultasStateChanged(evt);
             }
         });
 
@@ -91,91 +94,111 @@ public class JConsultaChamado extends javax.swing.JInternalFrame {
 
         box_Data.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Data" }));
 
-        box_status.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Status", "Todos", "Conc", "Pend" }));
+        box_Status.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Status", "Todos", "Concluido", "Pendente" }));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+        javax.swing.GroupLayout JPanelConsulaChamadoLayout = new javax.swing.GroupLayout(JPanelConsulaChamado);
+        JPanelConsulaChamado.setLayout(JPanelConsulaChamadoLayout);
+        JPanelConsulaChamadoLayout.setHorizontalGroup(
+            JPanelConsulaChamadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(JPanelConsulaChamadoLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
+                .addGroup(JPanelConsulaChamadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, JPanelConsulaChamadoLayout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(Button_Alterar))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(check_FIltros)
+                    .addComponent(jScrollPane1)
+                    .addGroup(JPanelConsulaChamadoLayout.createSequentialGroup()
+                        .addGroup(JPanelConsulaChamadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(JPanelConsulaChamadoLayout.createSequentialGroup()
+                                .addComponent(text_Consulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
+                                .addComponent(check_FIltros))
+                            .addGroup(JPanelConsulaChamadoLayout.createSequentialGroup()
                                 .addComponent(box_Patrimonio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
                                 .addComponent(box_Matricula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
                                 .addComponent(box_Data, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
-                                .addComponent(box_status, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(check_Consultas)
-                                .addGap(18, 18, 18)
-                                .addComponent(text_Consulta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addComponent(jScrollPane1))
-                        .addGap(0, 104, Short.MAX_VALUE)))
+                                .addComponent(box_Status, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 162, Short.MAX_VALUE)))
                 .addContainerGap())
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+        JPanelConsulaChamadoLayout.setVerticalGroup(
+            JPanelConsulaChamadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(JPanelConsulaChamadoLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(check_Consultas)
-                    .addComponent(text_Consulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(check_FIltros)
+                .addGroup(JPanelConsulaChamadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(text_Consulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(check_FIltros))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(JPanelConsulaChamadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(box_Patrimonio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(box_Matricula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(box_Data, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(box_status, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(box_Status, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 363, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(Button_Alterar)
                 .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(JPanelConsulaChamado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(JPanelConsulaChamado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void Button_AlterarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_AlterarMouseClicked
-       
-    }//GEN-LAST:event_Button_AlterarMouseClicked
-
-    private void check_ConsultasStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_check_ConsultasStateChanged
-        // TODO add your handling code here:
-    }//GEN-LAST:event_check_ConsultasStateChanged
-
     private void check_FIltrosStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_check_FIltrosStateChanged
-        // TODO add your handling code here:
+        if(check_FIltros.isSelected()){
+            box_Patrimonio.setVisible(true);
+            box_Matricula.setVisible(true);
+            box_Data.setVisible(true);
+            box_Status.setVisible(true);
+            validate();
+        }else{
+            box_Patrimonio.setVisible(false);
+            box_Matricula.setVisible(false);
+            box_Data.setVisible(false);
+            box_Status.setVisible(false);
+            validate();
+        }
     }//GEN-LAST:event_check_FIltrosStateChanged
 
-    private void text_ConsultaFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_text_ConsultaFocusGained
+    private void Button_AlterarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_AlterarMouseClicked
         // TODO add your handling code here:
+    }//GEN-LAST:event_Button_AlterarMouseClicked
+
+    private void text_ConsultaFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_text_ConsultaFocusGained
+        if(text_Consulta.getText().equals("Pesquisar...")){
+            text_Consulta.setText("");
+        }
     }//GEN-LAST:event_text_ConsultaFocusGained
 
     private void text_ConsultaFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_text_ConsultaFocusLost
-        // TODO add your handling code here:
+        if(text_Consulta.getText().equals("")){
+            text_Consulta.setText("Pesquisar...");
+        }
     }//GEN-LAST:event_text_ConsultaFocusLost
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Button_Alterar;
+    private javax.swing.JPanel JPanelConsulaChamado;
     private javax.swing.JList<String> List_Pendencias;
     private javax.swing.JComboBox<String> box_Data;
     private javax.swing.JComboBox<String> box_Matricula;
     private javax.swing.JComboBox<String> box_Patrimonio;
-    private javax.swing.JComboBox<String> box_status;
-    private javax.swing.JCheckBox check_Consultas;
+    private javax.swing.JComboBox<String> box_Status;
     private javax.swing.JCheckBox check_FIltros;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField text_Consulta;
